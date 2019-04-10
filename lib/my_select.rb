b0dedce2afa5a01740ecfa16f4176781c00459dc
Andrew Allen <asallen67@gmail.com>
2019-04-10 17:46:16 -0500

    1

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+	if collection.size > 0
+		selection=[]
+		bool=nil
+		for x in 0..collection.size-1
+			bool=yield(collection[x])
+			selection.push(collection[x]) if bool
+		end
+		selection
+	else
+		puts "This block should not run!"
+	end
 end
